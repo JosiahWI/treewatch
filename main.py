b76c3bbb97cddbe164c1651cde1e73234358577b
Josiah VanderZee <vanderzee3777@uwlax.edu>
2024-05-08 13:45:36 -0500

    Switch GUI to main thread
    This resolves an issue that was reported via email. The GUI must run on
    the main thread on OS X and possibly other platforms.

--- main.py
+++ main.py
@@ -16,32 +16,38 @@ def init_callback():
     global init_complete
     init_complete = True
 
-gui_thread = threading.Thread(target=gui.start_gui, args=(tree,init_callback))
-gui_thread.start()
+def console_loop():
+    # This busy wait is not the most efficient way to do this, but for this
+    # toy application I think it'll suffice for now. A better solution could
+    # be to set a semaphore or something and wait on that.
+    while not init_complete:
+        pass
 
-# This busy wait is not the most efficient way to do this, but for this
-# toy application I think it'll suffice for now. A better solution could
-# be to set a semaphore or something and wait on that.
-while not init_complete:
-    pass
+    while not (cmd := input("[add <i>, remove <i>, q(uit)]: ")).startswith("q"):
+        if cmd.startswith("add"):
+            if re.match("^add -?\d+$", cmd) is None:
+                print("Ill-formed add command.")
+                continue
+            value = int(cmd.split()[1])
+            with tree:
+                tree.add(value)
+        elif cmd.startswith("remove"):
+            if re.match("^remove -?\d+$", cmd) is None:
+                print("Ill-formed remove command.")
+                continue
+            value = int(cmd.split()[1])
+            with tree:
+                tree.remove(value)
+        else:
+            print(f"Unrecognized command \"{cmd}\".")
 
-while not (cmd := input("[add <i>, remove <i>, q(uit)]: ")).startswith("q"):
-    if cmd.startswith("add"):
-        if re.match("^add -?\d+$", cmd) is None:
-            print("Ill-formed add command.")
-            continue
-        value = int(cmd.split()[1])
-        with tree:
-            tree.add(value)
-    elif cmd.startswith("remove"):
-        if re.match("^remove -?\d+$", cmd) is None:
-            print("Ill-formed remove command.")
-            continue
-        value = int(cmd.split()[1])
-        with tree:
-            tree.remove(value)
-    else:
-        print(f"Unrecognized command \"{cmd}\".")
+    print("Finished. Please click the X on the GUI to finish shutting down if you haven't already done so.")
 
-print("Finished. Please click the X on the GUI to finish shutting down if you haven't already done so.")
-gui_thread.join()
+console_thread = threading.Thread(target=console_loop)
+console_thread.start()
+
+# The GUI must run on the main thread.
+# (https://github.com/google-deepmind/pysc2/issues/2)
+gui.start_gui(tree, init_callback)
+
+console_thread.join()
